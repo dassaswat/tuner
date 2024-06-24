4aea99e3e91cf8c722ed4742bd6f311eda7ac221
Saswat Das <saswatdas@Saswats-MacBook-Air.local>
2024-06-24 20:59:00 +0530

    chore: vercel speed insight

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import { init } from './controller';
+
 import './styles/global.css';
 
+injectSpeedInsights();
 (async () => await init())();
