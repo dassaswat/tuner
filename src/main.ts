ff60976dfbcc3e62a7627e45ed8faa854209b9e8
Saswat Das <saswatdas@Saswats-MacBook-Air.local>
2024-06-24 21:10:28 +0530

    fix: remove vercel insight and add speed insight and analytics to html file with script tag

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-import { injectSpeedInsights } from '@vercel/speed-insights';
 import { init } from './controller';
 
 import './styles/global.css';
 
-injectSpeedInsights();
 (async () => await init())();
